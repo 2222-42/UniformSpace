60ecba26dc25702c47c4abce39fb250b972b2c90
2222-42 <stephen0617@me.com>
2021-02-07 22:07:34 +0900

    chore(Metric_Uniform_Spaces): use class, not locale

--- Uniform Space/chapter1/Metric_Uniform_Spaces.thy
+++ Uniform Space/chapter1/Metric_Uniform_Spaces.thy
@@ -7,12 +7,12 @@ declare[[show_sorts=true]]
 declare[[show_consts=true]]
 
 (* https://mailmanbroy.informatik.tu-muenchen.de/pipermail/isabelle-dev/2019-April/016901.html *)
-locale psuedometic = 
-  fixes dist:: "'at \<Rightarrow> 'at \<Rightarrow> real"
+class psuedometic = 
+  fixes dist:: "'a \<Rightarrow> 'a \<Rightarrow> real"
   assumes dist_commut [simp]: "x \<in> \<UU> \<Longrightarrow> y \<in> \<UU> \<Longrightarrow> dist x y = dist y x \<and> dist x y \<ge> 0"
       and dist_triangle: "x \<in> \<UU> \<Longrightarrow> y \<in> \<UU> \<Longrightarrow> dist x y \<le> dist x z + dist y z"
 
-locale metric = psuedometic +
+class metric = psuedometic +
   assumes dist_0_if_not_eq : "x \<in> \<UU> \<Longrightarrow> y \<in> \<UU> \<Longrightarrow> x \<noteq> y \<Longrightarrow> dist x y > 0"
 
 end
